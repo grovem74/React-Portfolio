0c677971aa22d6a222e105cc3cc4ce1cbf4264b5
grovem74 <matthewg74@gmail.com>
2020-05-30 19:16:19 -0600

    Updates

---
+++ precache-manifest.a7d315e233527472ebad5385d5292d93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271a985627abb1fc4f6e449bd76b9c47",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b44c58684835b1965ac0",
+    "url": "/react-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de8cd03da15fe694f5db",
+    "url": "/react-portfolio/static/js/2.ebc468da.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/react-portfolio/static/js/2.ebc468da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44c58684835b1965ac0",
+    "url": "/react-portfolio/static/js/main.36b72739.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  }
+]);
